029d5206470104739a8eebcbdb4820d1cfcdb753
Kevin Pikkor <73058390+SWIITIGER@users.noreply.github.com>
2023-03-14 12:27:44 +0200

    Update SQLQuery1.sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,52 +1,45 @@
---- 1 tund
-
+-- komm
+-- teeb andmebaasi ehk db
 create database TARpe22
 
--- db kustutamine
+-- kustutamine db
 drop database TARpe22
 
 -- tabeli loomine
-
-
-
-
-
-
-
-
-
-id int not null primary key
+create table Gender
+(
+Id int not null primary key,
 Gender nvarchar(10) not null
 )
 
----andmete sisestamine
+--- andmete sisestamine
 insert into Gender (Id, Gender)
-values (2, 'Male')
+values (2, 'male')
 insert into Gender (Id, Gender)
-values (1, 'Female')
-insert into Gender (id, Gender)
-values (3, 'Unknown')
+values (1, 'female')
+insert into Gender (Id, Gender)
+values (3, 'unknown')
 
----sama Id väärtustega rida ei saa sisestada 
+-- sama Id v"rtusega rida ei saa sisestada
 select * from Gender
 
----teeme uue tabeli
+--- teeme uue tabeli
 create table Person
-( 
-   Id int not null primary key,
-   Name nvarchar(30),
-   Email nvarchar(30),
-   GenderId int
-)
+(
+    Id int not null primary key,
+    name nvarchar(30),
+    Email nvarchar(30),
+    GenderId int
+    )
 
 ---vaatame Person tabeli sisu
-select * from Person
+select * from person
 
----andmete sisestamine 
+---andmete sisestamine
 insert into Person (Id, Name, Email, GenderId)
 values (1, 'Superman', 's@s.com', 2)
 insert into Person (Id, Name, Email, GenderId)
-values (2, 'Wonderwoman', 'w@w.com', 1)
+values (2, 'wonderwoman', 'w@w.com.com', 1)
 insert into Person (Id, Name, Email, GenderId)
 values (3, 'Batman', 'b@b.com', 2)
 insert into Person (Id, Name, Email, GenderId)
@@ -54,47 +47,47 @@ values (4, 'Aquaman', 'a@a.com', 2)
 insert into Person (Id, Name, Email, GenderId)
 values (5, 'Catwoman', 'c@c.com', 1)
 insert into Person (Id, Name, Email, GenderId)
-values (6, 'Superman', 'ant"ant.com', 2)
-
+values (6, 'Antman', 'ant@ant.com', 2)
+insert into Person (Id, Name, Email, GenderId)
 values (8, NULL, NULL, 2)
 
-select * from Person
+select * from person
 
----võõrvõtme ühenduse loomine kahe tabeli vahel
+--vÃµÃµrvÃµtme Ã¼henduse loomine kahe tabeli vahel
 alter table Person add constraint tblPerson_GenderId_FK
-foreign key (GenderId) references Gender(Id)
+foreign key (GenderId) references Gender(id)
 
----kui sisestad uue rea andmeid ja ei ole sisetanud GenderId väärtust,
----see automaatselt sisestab tabelisse väärtuse 3 ja selleks on unknown
+---kui sisestad uue rea andmeid ja ei ole sisestanud GenderId all v''rtuse
+---see automaatselt sisestab tabelisse v''rtuse 3 ja selleks on unknwon
 alter table Person
 add constraint DF_Persons_GenderId
 default 3 for GenderId
 
-insert into Person (Id,Name, Email, GenderId)
-values (9'Ironman' ,'i@i.com', NULL)
+insert into Person (Id, Name, Email)
+values (9, 'Ironman', 'i@i.com')
 
 select * from Person
 
----piirangu maha võtmine
+-- piirangu maha v]tmine
 alter table Person
-drop constrait DF_Persons_GenderId
+drop constraint DF_Persons_GenderId
 
---lisame uue veeru
+---lisame uue veeru
 alter table Person
-add Age nvarchar(10)
+add age nvarchar(10)
 
----lisame vanuse piirangu sisetamisel
----ei saa lisada suuremat väärtust kui 801
+--- lisame vanus piirangu sisestamisel
+--- ei saa lisada suuremat v''rtust kui 801
 alter table Person
 add constraint CK_Person_Age check (Age > 0 and Age < 801)
 
 -- rea kustutamine
--- kui paned vale Id, siis ei muutu midagi
-delete from Person where Id = 9
+-- kui paned vale id, siis ei muuda midagi
+delete from Person where Id = 10
 
 select * from Person
 
--- kuidas uuendada andmeid tabelis
+--kuidas uuendada andmeid tabelis
 update Person
 set Age = 50
 where Id = 1
@@ -103,54 +96,124 @@ where Id = 1
 alter table Person
 add City nvarchar(50)
 
--- kõik, kes elavad Gothami linnas
+-- k]ik, kes elavad Gothami linnas
 select * from Person where City = 'Gotham'
--- kõik, kes ei ela Gothami linnas
-select * from Person where City != 'Gotham'
+-- k]ik, kes ei ela Gothami linnas
+select * from Person where City !='Gotham'
 -- teine variant
-select * from person where not City = 'Gotham'
+select * from Person where not City = 'Gotham'
 -- kolmas variant
-select * from person where City <> 'Gotham'
+select * from Person where City <> 'Gotham'
 
---näitab teatud vanusega inimesi
+-- n'itab teatud vanusega inimesi
 select * from Person where Age = 800 or Age = 35 or Age = 27
 select * from Person where Age in (800, 35, 27)
 
---näitab teatud vanusevahemikus olevaid inimesi
-select * from, Person where Age between 20 and 35
+-- n'itab teatud vanusevahemikus olevaid inimesi
+select * from Person where Age between 20 and 35
 
---wildcard e näitab kõik g-tähega linnad
+-- wildcard e nÃ¤itab kÃµik g-tÃ¤hega linnad
 select * from Person where City like 'g%'
--- näitab kõik emailid, milles on @ märk
+-- n'itab, k]ik emailid, milles on @ mÃ¤rk
 select * from Person where Email like '%@%'
 
---- näitab kõiki, kellel ei ole @-märki emailis
-select * from Person where Email not like '%"%'
+--- nÃ¤itab kÃµiki, kellel ei ole @-mÃ¤rki emailis
+select * from Person where Email not like '%@%'
 
- --- näitab, kellel on emailis ees ja peale @-märki 
- ---ainuly üks täht
- select * from Person where Email like '_@_.com'
+--- n'itab, kellel on Emailis ees ja peale @-mÃ¤rki
+-- ainult Ã¼ks tÃ¤ht
+select * from Person where Email like '_@_.com'
 
- -- kõik, kellel ei ole nimes esimene täht W,A,C
- select * from Person where name lke '[^WAC]%'
+-- k]ik, kellel ei ole nimes esimene t'ht W, A, C
+select * from Person where Name like '[^WAC]%'
 
- --- kes elavad Gothamis ja New Yorkis 
- select * from Person where (City = 'Gotham' or City = 'New York')
+--- kes elavad Gothamis ja New Yorkis
+select * from Person where (City = 'Gotham' or City = 'New York')
 
- -- kõik, kes elavad Gothamis ja New Yorkis ning
- -- alla 30 eluaastat
- select * from Person where (City = 'New York')
- and Age >= 30
+-- k]ik, kes elavad Gothamis ja New Yorkis ning
+-- alla 30 eluaastat
+select * from Person where
+(City = 'Gotham' or City = 'New York')
+and Age >= 30
 
- --- kuvab tähestikulises järjekorras inimesi
- --- ja võtab  aluseks nime
+--- kuvab t'hestikulises jÃ¤rjekorras inimesi
+--- ja vÃµtab aluseks nime
 
- select * from Person order by Name
- -- kuvab vastupidises järjekorras
- select * from Person order by Name desc
+select * from Person order by Name
+-- kuvab vastupidises jÃ¤rjekorras
+select * from Person order by name desc
+
+-- vÃµtab kolm esimest rida
+select top 3 * from Person
+
+--- muudab age muutuja int-ks ja nÃ¤itab vanuselises jÃ¤riestuses
+select * from Person order by cast(Age as int)
+
+--- kÃµikide isikute koondvanus
+select SUM(CAST(Age as int))from Person
+
+--- nÃ¤itab, kÃµige nooremat isikut
+select MIN(CAST(Age as int)) from Person
+
+--- nÃ¤itab, kÃµige nooremat isikut
+select MIN(CAST(Age as int)) from Person
+
+-- nÃ¤eme konkreetsetes linnades olevate isikute koondvanust
+-- enne oli age string, aga pÃ¤ringu ajal muutsime selle int-ks
+select City, SUM(CAST(Age as int)) as TotalAge from Person groub by City
+
+--kuidas saab koodiga muuta tabeli andmetÃ¼Ã¼pi ja selle pikkust
+alter table Person
+alter column Name nvarchar(25)
+
+alter table Person
+alter column Age int
 
 
- -- võtab kolm esimest rida 
- select top 3 * from Person
+--kuvab esimeses reas vÃ¤lja toodud jÃ¤rjestused ja muudab Age-i TotalAge-ks
+--teeb jÃ¤rjestuse vaatesse: City, GenderId ja jÃ¤rjestab omakorda City veeru
+select City, GenderId, SUM(Age) as TotalAge from Person
+group by City, GenderId order by City
 
- --- 2 tund
+--nÃ¤itab, et mitu rida on selles tabelis
+select COUNT(*) from Person
+
+
+---veergude lugemine
+SELECT count(*)
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'Person'
+
+---nÃ¤itab tulemust, et miu inimest on genderId 
+---vÃ¤Ã¤rtusega 2 konkreetses linnas
+---arvutab kokku vanuse
+select GenderId, City, SUM(Age) as TotalAge, COUNT(Id) as [Total Person(s)]
+from Person
+where GenderId = '2'
+group by GenderId, City
+
+---nÃ¤itab, et mitu inimest on vanemad, kui 41 ja kui palju igas linnas
+select GenderId, City, SUM(Age) as TotalAge, COUNT(Id) as [Total Person(s)]
+from Person
+group by GenderId, City having SUM(Age) > 41
+
+--loome uue tabeli
+create table Department
+(
+Id int primary key, key
+DepartmentName nvarchar(50),
+[Location] nvarchar(50),
+DepartmentHead nvarchar(50)
+)
+
+create table Employees
+(
+Id int primary key,
+Name nvarchar(50),
+Gender nvarchar(50),
+Salary nvarchar(50),
+DepartmentId int
+)
+
+insert into Employs (id, Name, Gender, Salary, DepartmentId)
+values (1, 'Tom', 'Male', '4000', 'IT')
